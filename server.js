1824ecd7ed3fd23dfba4fe49a4fec00d8f8d64fc
Apubra <apurbadaria8@gmail.com>
2021-03-19 18:45:52 +0600

    change port

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ const app = express();
 // Index route
 app.get('/', (req, res) => res.send('Hi, I am from Azure VM'));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
